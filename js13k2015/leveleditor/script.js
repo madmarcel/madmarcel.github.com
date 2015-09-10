3fe107e62034cf57d56fbeef9ee9b9529651224b
Marcel van de Steeg <mvandest@cisco.com>
2015-09-11 00:45:28 +1200

    Almost there

--- js13k2015/leveleditor/script.js
+++ js13k2015/leveleditor/script.js
@@ -8,6 +8,14 @@ $(document).ready(function(){
 					];
 
 var data = [
+'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb..................................TTTTVTTTTTTT333GbbbbgttttVttttttVtttVttGbgtTTTVtTTTTVTTTGbbbbbbbbb......................................D.......333Gbbbbg....D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D.......353Gbbbbg1...D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D......L3%3Gbbbbg11..D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D......&&&&Gbbbbg&&&.D...&..D...D..Gbg....D.....D...Gbbbbbbbbb......................................D....l&&&&&Gbbbbg333.D&.....D...D..Gbg....D.....D...Gbbbbbbbbb......................................D....&&&&&&Gbbbbg333.D......D...D..AAA....D.....D...Gbbbbbbbbb......................................D...&&&&&&&Gbbbbg333&D......D...D..AAA....C..U..C...Gbbbbbbbbb......................................D..&&&&&&&&Gbbbbg353.D...$..C.u.Cl$AAA....&&&&&&&...Gbbbbbbbbb...................................~.nC.&&&&&&&&&Gbbbbg3^3.C...n&&&&&&&&&AAA...&&&&&&&&&..Gbbbbbbbbb..................................hhhhhhhhhhhhhhhBbbbbbmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmbbbbbbbbbb..................................1',
+'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb..................................TTTTVTTTTTTT333GbbbbgttttVttttttVtttVttGbgtTTTVtTTTTVTTTGbbbbbbbbb......................................D.......333Gbbbbg....D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D.......353Gbbbbg1...D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D......L3%3Gbbbbg11..D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D......&&&&Gbbbbg&&&.D......D...D..Gbg....D.....D...Gbbbbbbbbb......................................D....l&&&&&Gbbbbg333.D&.....D...D..Gbg....D.....D...Gbbbbbbbbb......................................D....&&&&&&Gbbbbg333.D......D...D..3......D.....D...Gbbbbbbbbb......................................D...&&&&&&&Gbbbbg333&D......D...D..3......C..U..C...Gbbbbbbbbb......................................D..&&&&&&&&Gbbbbg353.D...$..C.u.Cl$3......&&&&&&&...Gbbbbbbbbb...................................~.nC.&&&&&&&&&Gbbbbg3^3.C...n&&&&&&&&&3.....&&&&&&&&&..Gbbbbbbbbb..................................hhhhhhhhhhhhhhhBbbbbbmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmbbbbbbbbbb..................................1',
+'....ImmmJBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB........................................i.~.BBBBBBEaaaaaaaaaaWBBBBBBBBBBBBBBBBEaaaaaaaaaWBBBBEaaaaWB........................................BAAAWBBBBEaaaaaaaaaaaaBBBBBBBBBBBBBEaaaaaaa09aaaaWBBBaaaaaaa........................................BAAAAWBBEaaaaaa$-09a$aWBBBBBBBBBBEaaaaa]aYBBBBZaaaBBBZaaaaaa........................................BAAAAAaaaaaaaaaYBBBByaaWBBBBBBBBEaaaaYBBBBBBBBBZaaWBBBZaa09a........................................BAAAAAaaaaaaaaaWBBBBEaaaaaaaaaaaaa0YBBBBBBBBBBBBZaaBBBBBBBBB........................................BAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaYBBBBBBEaaaaWBBBaaBBBBBBBBB........................................BAAAAAaaaaaaaaaaaaaaaaaaaaaaa09YBBBBBEaaaaaaaaWBEaaBBBBEaaaa........................................BZAAAAaa90aaaaaaaaaaaaa0a0$aYBBBBBBBBaaaaaa0aaaaaaaWBEaaaaaa........................................BBZAAAaYBBZa)aaaaaaaaaYBBBaaBBBBBBBBBaaaaaYBZaaaaaaaaaaa0a09........................................BBBZ9YBBBBBBBBZa09a{aYBBBBaaBBBBBBBBBZ09YBBBBZaa+aa09aYBBBBB........................................BBBBBBBBBBBBBBBBBBBBBBBBBBaaBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB........................................0',
+'....ImmmJBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB........................................i.~.BBBBBBEaaaaaaaaaaWBBBBBBBBBBBBBBBBEaaaaaaaaaWBBBBEaaaaWB........................................BAAAWBBBBEaaaaaaaaaaaaBBBBBBBBBBBBBEaaaaaaaaaaaaaWBBBaaaaaaa........................................BAAAAWBBEaaaaaa$-aaa$aWBBBBBBBBBBEaaaaa]aYBBBBZaaaBBBZaaaaaa........................................BAAAAAaaaaaaaaaYBBBByaaWBBBBBBBBEaaaaYBBBBBBBBBZaaWBBBZaaaaa........................................BAAAAAaaaaaaaaaWBBBBEaaaaaaaaaaaaaaYBBBBBBBBBBBBZaaBBBBBBBBB........................................BAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaYBBBBBBEaaaaWBBBaaBBBBBBBBB........................................BAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaYBBBBBEaaaaaaaaWBEaaBBBBEaaaa........................................BZAAAAaaaaaaaaaaaaaaaaaaaa$aYBBBBBBBBaaaaaaaaaaaaaaWBEaaaaaa........................................BBZAAAaYBBZa)aaaaaaaaaYBBBaaBBBBBBBBBaaaaaYBZaaaaaaaaaaaaaaa........................................BBBZAYBBBBBBBBZaaaa{aYBBBBaaBBBBBBBBBZaaYBBBBZaa+aaaaaYBBBBB........................................BBBBBBBBBBBBBBBBBBBBBBBBBBaaBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB........................................0',
+'..............................................KLn................................................................................................Gmmmg.................................nlkn........................*..................................bbb.................................Immmmi.....................................................777..bbb...............................nIJsoooji.......888...........$..(...$.@.....................333..bbb..777.........................ImJsbbbboji......444............VcUucv$.Xdx.$...]$............353.}bbb..333......................nnIJsobbbbbboji.....464..............Tt.........VUuv............n3%3NIbbbi.353.........*...........ImmJsbbbbbbbbboji....444..............Tt..........Tt............ImmmmmJbbbji3^3....................IJsoobbbbbbbbbbboji...444..............Tt......1...Tt..kL.......IJsooooobbbojmmmmg.................IJsbbbbbbbbbbbbbbboji..464..~n.lkn.]1+n.Tt.N.n-11{.NTtnImmi)$$nLIJsbbbbbbbbbbooooo.................IJsbbbbbbbbbbbbbbbbboji.4!4.mmmmmmmmmmmmmmMMMMMMMMMMMMMMJsojmgGmmJsbbbbbbbbbbbb[...................GJsbbbbbbbbbbbbbbbbbbbojmmmmmooooooooobbbboooooooooooooooobboofFooobbbbbbbbbbbbbfffffffffffffffffffffobbbbbbbbbbbbbbbbbbbbboooooo0',
+//'..............................................KLn................................................................................................Gmmmg.............................................................*..................................bbb............................................................................................777..bbb.............................................................$..(...$.@.....................333..bbb..............................................................VcUucv$.Xdx.$...]$............353.}bbb................................................................Tt.........VUuv............n3%3NIbbb................................................................Tt..........Tt............ImmmmmJbbb................................................................Tt......1...Tt..kL.......IJsooooobbb....................................................~n.lkn.]1+n.Tt.N.n-11{.NTtnImmi)$$nLIJsbbbbbbbbb...................................................mmmmmmmmmmmmmmMMMMMMMMMMMMMMJsojmgGmmJsbbbbbbbbbb...................................................ooooooooobbbboooooooooooooooobboofFooobbbbbbbbbbb...................................................0',
+'..........................................b...................................................................................................b...................................................................................................b......................................................................................888..........b...................................................................$..)...$.@.........444..........b....................................................................VcUucv$.Xdx......$464..........b......................................................................Tt.........VUuv.444........nNb......................................................................Tt..........Tt..444.......Immb......................................................................Tt..........Tt..464......IJsbb..........................................................~n.lkn.n1.n+Tt.N.n.-.2.NTtn.4!4n..nLIJsbbb.........................................................mmmmmmmmmmmmmmMMMMMMMMMMMMMMMMMMmgGmmJsbbbb.........................................................ooooooooobbbboooooooooooooooooooofFooobbbbb.........................................................0',
+'.........................................................................................................................................................................................................................................................................................................................................888..............................................................................$..).(.$.@.........444...............................................................................VcUucv$.Xdx......$464.................................................................................Tt..............444........nN.......................................................................Tt..............444.......Imm.......................................................................Tt..............464......IJsb...........................................................~n..knnn2.n+TtN....-.12N.N..4!4n..n.IJsbb..........................................................mmmmmmmmmmmmmmMMMMMMMMMMMMMMMMMMmgGmmJsbbb..........................................................ooooooooobbbboooooooooooooooooooofFooobbbb..........................................................0',
 '.................$%............................................................................................Ihhhhhhhhhhii......................................................................................Iaaaaaaaaaaaaii....................................................................................Ia..aaaaaaaa..aai..................................................................................Iaa..aaaaaaaa..aaai................................................................................Iaaaaaaaaaaaaaaaaaaai..............................................................................Iaaaaaaaaaaaaaaaaaaaaaa............................................................................Iaaaaaaaaaaaaaaaaaaaaaaaa......................................................................~...Iaaaaaaaaaaaaaaaaaaaaaaaaaa.....................................................................mmmIaaaaaa..............aaaaaaaa....................................................................baaaaaaaAAAAAAAbbbbbbbbbbbbbbbbbbbbbbbbbbbb.........................................................bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb.........................................................0',
 '.........................................................................................................................................................................................................................................................................................................................................888..............................................................................$......$.@.........444...............................................................................VcUucv$.Xdx......$464....................................................................................aaaaa........444........nN....................................................................nn....aaaaa........444.......Imm...................................................................Immi...aYBya........464......IJsb...........................................................~n..kL.IJsoji.NzbbBZ.12N.N..4!4n..n.IJsbb..........................................................mmmmmmmmJsbbojMMMMMMMMMMMMMMMMMMmgGmmJsbbb..........................................................ooooooooobbbboooooooooooooooooooofFooobbbb..........................................................0',
 '...................................................................................................................*.................................VcUucv..........................................................$..).$................................Tt..................n.nKLN.....................................VUuv....$n+N$...............nLN......Tt.................Immmmmmi...................888...............Tt.....Ghhhg.....]n$......GHHHg.....Tt................IJsoooooji..................444.....nnN.......Tt..............Immg................TtN.$n.].n.lN$$nlIJsbbbbbboji.LnN)))lk$.......464....Ghhhg......Ttn............IJoo................ImmmgGhhhhhhhhgGmmJswaaaaaaeojmmmmmmmmmi.......444..............Immmi..........IJsbblkn.......N....IJsoo............ooowaaaaaaaaeoooooooooob2......464.nlk~N..n....IJsooji........IJsbbbmmmg....GmmmilIJsbbb...........Ibbbaaaaaaaaaaaaaaaaaaaaa22.....4!4mmmmmmmmmg$kIJsbbboji......IJsbbbbooon.L...ooojmJsbbbb.-....KL..IJbbbyaaaaaaaaaaaaaaaaaaaa222.N.Immmooooooooo.GmJsbbbbboji-nkLIJsbbbbbbbbmmmg..bbbooobbbbbmmmmmmmmmmJsbbbbbbbbbbbyaazbbyaaaaazmmmmmmJsoobbbbbbbbbffoobbbbbbbojmmmmJsbbbbbbbbboooFFFbbbbbbbbbbbooooooooooobbbbbbbbbbbbbbbbbbbbbbbbbbbbbboobbb0',
@@ -22,7 +30,7 @@ var data = [
 '................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................................0',
 
 ];
-	var metachars = [ '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '[', ']', '{', '}', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+	var metachars = [ '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '[', ']', '{', '}', '2', '1', '3', '4', '5', '6', '7', '8', '9', '0' ];
 
 	// build table
 	var height = 12;
@@ -70,25 +78,25 @@ var data = [
 
 		l.empty();
 
-		for (var i = 0; i < 26; i++) {
-			var style = 'width: 64px; height: 64px; background: url(meta.png) ' + ( i * -64 ) + 'px 0px; background-size: 2600%;';
-			l.append('<li data-id="' + metachars[i] + '" class="tile" style="' + style + '"></li>');
+		for (var i = 0; i < metachars.length; i++) {
+			var style = 'width: 64px; height: 64px; background: url(meta.png) ' + ( i * -64 ) + 'px 0px; background-size: 2700%;';
+			l.append('<li data-meta="a" data-id="' + metachars[i] + '" class="tile" style="' + style + '"></li>');
 		}
 
-		var l = $('div.fixed ul#meta2');
+		/* var l = $('div.fixed ul#meta2');
 		l.empty();
 
 		for (var i = 0; i < 26; i++) {
 			var style = 'width: 16px; height: 16px; background: url(spritesheet.png) ' + ( i * -16 ) + 'px ' + (2 * 16) + 'px;';
 			l.append('<li data-id="' + metachars[i] + '" class="tile" style="' + style + '"></li>');
-		}
-		var l = $('div.fixed ul#meta3');
+		} */
+		/* var l = $('div.fixed ul#meta3');
 		l.empty();
 
 		for (var i = 0; i < 26; i++) {
 			var style = 'width: 16px; height: 16px; background: url(spritesheet.png) ' + ( i * -16 ) + 'px ' + (3 * 16) + 'px;';
 			l.append('<li data-id="' + metachars[i] + '" class="tile" style="' + style + '"></li>');
-		}
+		} */
 
 		// handle li click
 		$("div.fixed ul li").click(function() {
@@ -152,7 +160,7 @@ var data = [
 
 			if ( metachars.indexOf( value ) > -1 ) {
 				$(this).css('background', 'url(meta.png) ' + ( metachars.indexOf( value ) * -64) + 'px 0px');
-					$(this).css('background-size', '2600%');
+					$(this).css('background-size', '2700%');
 					$(this).removeClass('flipped');
 			}
 			else
@@ -192,6 +200,15 @@ var data = [
 		console.log('-----------[ LEVEL ' + level + ' ]------------');
 		console.log( result );
 		console.log('---------------------------------------------');
+		console.log( 'Total chars: ' + result.length );
+
+		/*var nr = prepare( result );
+
+		var encoded = enc( nr );
+
+		console.log( encoded );
+		console.log('---------------------------------------------');
+		console.log( 'Total chars: ' + encoded.length ); */
 	});
 
 	var setLevelIndicator = function() {
@@ -225,7 +242,7 @@ var data = [
 
 				if ( metachars.indexOf( value ) > -1 ) {
 					$(this).css('background', 'url(meta.png) ' + ( metachars.indexOf( value ) * -64) + 'px 0px');
-					$(this).css('background-size', '2600%');
+					$(this).css('background-size', '2700%');
 					$(this).removeClass('flipped');
 				}
 				else
@@ -276,7 +293,7 @@ var data = [
 	$("#next").click(function(e) {
 		e.preventDefault();
 
-		if ( level < 8) {
+		if ( level < data.length) {
 			level++;
 			loadData();
 		}
@@ -352,4 +369,46 @@ var data = [
 
 	console.log( 'no match found' );
 	*/
+
+	var prepare = function( d ) {
+		d = String(d);
+
+		var r = '';
+
+		for (var i = 0; i < d.length; i++ ) {
+			r += '' + d[i].charCodeAt(0);
+		}
+
+		return r;
+	}
+
+	var enc = function(num){
+		// Stringify our number in case it was input as an integer.
+		num = String(num);
+
+		// Keep track of our encoded chunks.
+		var encodedChunks = [];
+
+		// Continue until we've processed the entire string.
+		while(num.length){
+			// Start somewhere.
+			var splitPosition = 8;
+
+			// Try incrementally larger pieces until we get one that's exectly
+			// 8 characters long.
+			var encodedNum = '';
+			while(encodedNum.length < 8 && splitPosition < num.length && splitPosition < 15){
+				// toString(36) converts decimal to base36.
+				encodedNum = Number(num.substr(0, ++splitPosition)).toString(36);
+			}
+
+			// Push our chunk onto the list of encoded chunks and remove these
+			// digits from our string.
+			encodedChunks.push(encodedNum);
+			num = num.substr(splitPosition);
+		}
+
+		// Return a big ol' string.
+		return encodedChunks.join('');
+	}
 });
